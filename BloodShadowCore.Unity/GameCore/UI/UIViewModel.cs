77b14725e2a4e6d586961a5b59b1a27decad12c3
Lucifer <64275869+artem-python@users.noreply.github.com>
2025-05-15 18:27:41 +0300

    asd

--- BloodShadowCore.Unity/GameCore/UI/UIViewModel.cs
+++ BloodShadowCore.Unity/GameCore/UI/UIViewModel.cs
@@ -3,8 +3,5 @@ using UnityEngine;
 
 namespace BloodShadow.Unity.GameCore.UI
 {
-    public abstract class UIViewModel : UIViewModel<GameObject>
-    {
-        protected UIViewModel(UIController controller) : base(controller) { }
-    }
+    public abstract class UIViewModel : UIViewModel<GameObject> { }
 }
